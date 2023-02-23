a3dfc1cf97344e00a769d18baa1d6f21042a69e1
Nya Tyus <nyastyus@gmail.com>
2023-02-23 00:21:18 -0500

    pass positive test case for manager test

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,20 +1,20 @@
-// const Manager = require("../lib/Manager");
+const Manager = require("../lib/Manager");
 
-// describe("Manager", () => {
-//   describe("Initialization", () => {
-//     it("should create an object with a name, id, email, and office number", () => {
-//       const employee = new Manager("Jane", 1234, "email@email.com", 105);
+describe("Manager", () => {
+  describe("Initialization", () => {
+    it("should create an object with a name, id, email, and office number", () => {
+      const employee = new Manager("Jane", 1234, "email@email.com", 105);
 
-//       expect(employee.name).toEqual("Jane");
-//       expect(employee.id).toEqual(1234);
-//       expect(employee.email).toEqual("email@email.com");
-//       expect(employee.github).toEqual(105);
-//     });
+      expect(employee.name).toEqual("Jane");
+      expect(employee.id).toEqual(1234);
+      expect(employee.email).toEqual("email@email.com");
+      expect(employee.officeNumber).toEqual(105);
+    });
 
-//     it("should throw an error if provided no arguments", () => {
-//       const example = () => new Manager();
+    //     it("should throw an error if provided no arguments", () => {
+    //       const example = () => new Manager();
 
-//       expect(example).toThrow();
-//     });
-//   });
-// });
+    //       expect(example).toThrow();
+    //     });
+  });
+});
