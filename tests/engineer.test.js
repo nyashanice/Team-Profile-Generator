812f23466599b1658ffc1fdcb86e3b190bf9188f
Nya Tyus <nyastyus@gmail.com>
2023-02-23 00:19:08 -0500

    pass positive test case for engineer test

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,20 +1,20 @@
-// const Engineer = require("../lib/Engineer");
+const Engineer = require("../lib/Engineer");
 
-// describe("Engineer", () => {
-//   describe("Initialization", () => {
-//     it("should create an object with a name, id, email, and github", () => {
-//       const employee = new Engineer("Jane", 1234, "email@email.com", "janedoe");
+describe("Engineer", () => {
+  describe("Initialization", () => {
+    it("should create an object with a name, id, email, and github", () => {
+      const employee = new Engineer("Jane", 1234, "email@email.com", "janedoe");
 
-//       expect(employee.name).toEqual("Jane");
-//       expect(employee.id).toEqual(1234);
-//       expect(employee.email).toEqual("email@email.com");
-//       expect(employee.github).toEqual("janedoe");
-//     });
+      expect(employee.name).toEqual("Jane");
+      expect(employee.id).toEqual(1234);
+      expect(employee.email).toEqual("email@email.com");
+      expect(employee.github).toEqual("janedoe");
+    });
 
-//     it("should throw an error if provided no arguments", () => {
-//       const example = () => new Engineer();
+    //     it("should throw an error if provided no arguments", () => {
+    //       const example = () => new Engineer();
 
-//       expect(example).toThrow();
-//     });
-//   });
-// });
+    //       expect(example).toThrow();
+    //     });
+  });
+});
